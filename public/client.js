5ac1bc2b28a69c7e480a4fa401f645444129e255
Isaac Del Castillo <52974036+IsaacDC@users.noreply.github.com>
2024-08-19 12:56:04 -0400

    Update client.js

--- public/client.js
+++ public/client.js
@@ -13,8 +13,8 @@ document.addEventListener("DOMContentLoaded", () => {
   let lastY = 0;
   let color = "#000000";
   let strokeWidth = 5;
-  const canvasWidth = 3440;
-  const canvasHeight = 1440;
+  const canvasWidth = 2560;
+  const canvasHeight = 1080;
 
   // Set up canvas
   canvas.width = canvasWidth;
